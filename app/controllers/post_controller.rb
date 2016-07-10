3f863ca08ddea1e3c461d4ecf1e0ae045f04258b
Rittersm@gmail.com <rittersm@Seans-MacBook-Pro.local>
2016-07-10 11:54:38 -0400

    Page layout finalized

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,12 +2,14 @@ class PostController < ApplicationController
 
   def index
     @page_title = "Main Page"
-    @posts = Post.all.sort_by{|post| post.created_at}.first
+    @posts = Post.all.sort_by{|post| post.created_at}.last
+    @post_images = Post.all.sort_by{|image| image.created_at} - [@posts]
   end
 
   def show
     @posts = Post.all.detect{|post| post.id == params[:id].to_i}
     @page_title = @posts.title
+    @post_images = Post.all.sort_by{|image| image.created_at} - [@posts]
   end
 
 end
