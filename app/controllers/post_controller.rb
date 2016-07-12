dc3939e8c924f497296937f186c906cc342c90a9
rittersm <rittersm@gmail.com>
2016-07-12 16:35:58 -0400

    only tests remain

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,14 +2,14 @@ class PostController < ApplicationController
 
   def index
     @page_title = "Main Page"
-    @posts = Post.all.sort_by{|post| post.created_at}.last
-    @post_images = Post.all.sort_by{|image| image.created_at} - [@posts]
+    @posts = Post.all.order(created_at: :desc).first
+    @post_images = Post.all.order(created_at: :desc).offset(1)
   end
 
   def show
     @posts = Post.all.detect{|post| post.id == params[:id].to_i}
     @page_title = @posts.title
-    @post_images = Post.all.sort_by{|image| image.created_at} - [@posts]
+    @post_images = Post.all.order(created_at: :desc).where("id != ?", params[:id].to_i)
   end
 
 end
